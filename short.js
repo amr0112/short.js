0e9859408d8ddc7efd7ba412ebbc303a56d836f9
amr0112 <amr.syr0112@gmail.com>
2020-03-31 08:32:46 +0200

    Delete short.js

--- short.js
+++
@@ -1,5 +0,0 @@
-<script type='text/javascript'>
-//<![CDATA[
-protected_links = "yourdomain.com, sub.domain.com, anotherdomain.com,youtube.com,facebook.com,google.com"; (function() { var sl = document.createElement('script'); sl.type = 'text/javascript'; sl.async = true; sl.src = 'https://cdn.rawgit.com/antoncabon/shared-file/master/autocovert-usesafelink.js'; (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(sl); })();
-//]]>
-</script>
